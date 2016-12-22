93d6b41c0792050cc6390b9a46e00f3252e29fb1
genadis <genadis.pub@gmail.com>
2016-12-23 01:28:14 +0200

    fix mdl-spinner for rtl websites

--- src/components/loading/mdl-spinner.js
+++ src/components/loading/mdl-spinner.js
@@ -1,6 +1,13 @@
-import {bindable, customElement} from 'aurelia-framework';
+import {inject, bindable, customElement, DOM} from 'aurelia-framework';
 
 @customElement('mdl-spinner')
+@inject(DOM.Element)
 export class MDLSpinner {
   @bindable active = true;
+
+  constructor(element) {
+    this.element = element;
+    // make sure spinner dir is always ltr (rtl breaks it).
+    element.style.direction = 'ltr';
+  }
 }
